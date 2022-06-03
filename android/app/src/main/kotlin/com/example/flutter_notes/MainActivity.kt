4eb8c2eff1370be6407f4588a81e579387d35a15
Yanow0 <yan.parmentier@gmail.com>
2022-06-03 15:51:04 +0200

    fix: insert notes after db initialisation

--- android/app/src/main/kotlin/com/example/flutter_notes/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_notes/MainActivity.kt
@@ -1,16 +1,5 @@
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.imagepicker.ImagePickerPlugin
-import com.tekartik.sqflite.SqflitePlugin
+import io.flutter.embedding.android.FlutterActivity
+package com.example.flutter_notes
 
-
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        ImagePickerPlugin.registerWith(
-                registrarFor("io.flutter.plugins.imagepicker.ImagePickerPlugin"))
-        SqflitePlugin.registerWith(registrarFor("com.tekartik.sqflite.SqflitePlugin"))
-
-    }
-
+class MainActivity: FlutterActivity() {
 }
--- lib/app/screens/home/home_screen.dart
+++ lib/app/screens/home/home_screen.dart
@@ -15,21 +15,22 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     NotesDatabase notesDatabase = NotesDatabase();
-    notesDatabase.initDatabase();
-    Note note = Note(
-      title: 'Note 1',
-      content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    );
+    notesDatabase.initDatabase().then((_) {
+      Note note = Note(
+        title: 'Note 1',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      );
 
-    Note note2 = Note(
-      title: 'Note 2',
-      content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    );
+      Note note2 = Note(
+        title: 'Note 2',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      );
 
-    notesDatabase.insertNote(note);
-    notesDatabase.insertNote(note2);
+      notesDatabase.insertNote(note);
+      notesDatabase.insertNote(note2);
+    });
 
     return Scaffold(
       appBar: AppBar(
