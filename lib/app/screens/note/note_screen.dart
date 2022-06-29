e543da388061c728eb223316633475ee25fe72ce
Enzo Foucaud <foucauden@gmail.com>
2022-06-29 14:41:02 +0200

    New: Add camera function

--- lib/app/screens/note/note_screen.dart
+++ lib/app/screens/note/note_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notes/app/modules/note/bloc/note_bloc.dart';
 import 'package:flutter_notes/app/modules/note/bloc/note_events.dart';
 import 'package:flutter_notes/app/modules/note/bloc/note_states.dart';
 import 'package:flutter_notes/core/di/locator.dart';
-
 
 class NoteScreen extends StatefulWidget {
   const NoteScreen({Key? key}) : super(key: key);
@@ -15,16 +15,25 @@ class NoteScreen extends StatefulWidget {
 }
 
 class _NoteScreenState extends State<NoteScreen> {
-
   NoteBloc noteBloc = locator<NoteBloc>();
+
+  // Allow you to open camera and take picture
+  List<XFile> pickedImages = [];
+  launchCamera() async {
+    final ImagePicker picker = ImagePicker();
+    var pickedImage = await picker.pickImage(source: ImageSource.camera);
+    print(pickedImage?.path); // ignore: avoid_print
+    pickedImages.add(pickedImage!);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text('Note'),
-    ),
-    body: BlocBuilder<NoteBloc, NoteState>(
+      ),
+      body: BlocBuilder<NoteBloc, NoteState>(
           buildWhen: (previous, current) => previous is NoteListLoadingState,
           builder: (_, state) {
             if (state is NoteListSuccessState) {
@@ -32,8 +41,7 @@ class _NoteScreenState extends State<NoteScreen> {
                 children: state.notes
                     .map((note) => InkWell(
                           onTap: () {
-                            noteBloc
-                                .add(GetAllNotesEvent());
+                            noteBloc.add(GetAllNotesEvent());
                           },
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
@@ -60,7 +68,6 @@ class _NoteScreenState extends State<NoteScreen> {
             }
             return Container();
           }),
-
     );
   }
 }
