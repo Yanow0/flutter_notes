925cc08bc233364ff81ec4e51611b947799d1d8a
Enzo Foucaud <foucauden@gmail.com>
2022-06-30 15:04:49 +0200

    New: Add camera + image

--- lib/app/screens/note/note_screen.dart
+++ lib/app/screens/note/note_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -11,13 +13,16 @@ class NoteScreen extends StatefulWidget {
 
 class _NoteScreenState extends State<NoteScreen> {
   // Allow you to open camera and take picture
-  List<XFile> pickedImages = [];
-  launchCamera() async {
+  XFile? _image;
+  void getImage(ImageSource imageSource) async {
     final ImagePicker picker = ImagePicker();
-    var pickedImage = await picker.pickImage(source: ImageSource.camera);
-    print(pickedImage?.path); // ignore: avoid_print
-    pickedImages.add(pickedImage!);
-    setState(() {});
+    XFile? imageFile = await picker.pickImage(
+      source: imageSource,
+    );
+    if (imageFile == null) return;
+    setState(() {
+      _image = imageFile;
+    });
   }
 
   @override
@@ -26,29 +31,66 @@ class _NoteScreenState extends State<NoteScreen> {
       appBar: AppBar(
         title: const Text('Ajouter une Note'),
       ),
-      body: ListView(children: [
-        ElevatedButton(
-            onPressed: () {
-              launchCamera();
+      body: SingleChildScrollView(
+        child: Column(children: [
+          if (_image != null)
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              width: MediaQuery.of(context).size.width,
+              height: 250.0,
+              child: Stack(
+                children: [
+                  Image.file(File(_image!.path)),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        height: 20.0,
+                        width: 32.0,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.white),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              _image = null;
+                            });
+                          },
+                          child: const Icon(
+                            Icons.delete,
+                            size: 32.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          TextField(
+            decoration: const InputDecoration(
+                labelText: 'Titre', border: InputBorder.none),
+            onChanged: (text) {
+              print('Title: $text');
             },
-            child: const Text('Prendre une photo')),
-        TextField(
-          decoration: const InputDecoration(
-              labelText: 'Titre', border: InputBorder.none),
-          onChanged: (text) {
-            print('Title: $text');
-          },
-          maxLines: null,
-          textCapitalization: TextCapitalization.sentences,
-        ),
-        TextField(
-          decoration: const InputDecoration(
-              labelText: 'Contenu', border: InputBorder.none),
-          onChanged: (text) {
-            print('Content: $text');
-          },
-        ),
-      ]),
+            maxLines: null,
+            textCapitalization: TextCapitalization.sentences,
+          ),
+          TextField(
+            decoration: const InputDecoration(
+                labelText: 'Contenu', border: InputBorder.none),
+            onChanged: (text) {
+              print('Content: $text');
+            },
+          ),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getImage(ImageSource.camera);
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
     );
   }
 }
